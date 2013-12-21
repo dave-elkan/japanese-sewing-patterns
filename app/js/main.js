5a6867739797d116bafb480d58fc2e49070d09eb
Dave Elkan <dave@edave.net>
2013-12-21 12:35:43 -0800

    jshint

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,16 @@
 $(function() {
-    $(".fancybox").fancybox({
+    $('.fancybox').fancybox({
         prevEffect		: 'none',
         nextEffect		: 'none',
         closeBtn		: true
     });
 
     $(document).on('click', '.ebook-nav a', function(e) {
-        var scrollTo = $(e.target).attr("href");
+        var scrollTo = $(e.target).attr('href');
         var $scrollTo = $(scrollTo);
         if ($scrollTo.length) {
             window.smoothScrollTo(0, $scrollTo.offset().top, {
-                behavior: "smooth"
+                behavior: 'smooth'
             });
         }
     });
