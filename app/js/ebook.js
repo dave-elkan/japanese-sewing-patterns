02a5877b4e73a3dc5bfe41e72d97411839a87b5a
Dave Elkan <dave@edave.net>
2014-03-29 12:27:37 -0700

    Remove e-junkie and dpd instead. Track purchase clicks.

--- app/js/ebook.js
+++ app/js/ebook.js
@@ -1,4 +1,15 @@
 $(function() {
+
+    $(document).on('click', 'input.trial', function(e) {
+        ga('send', 'event', 'Samples', 'submit', 'E-book sample');
+    });
+
+    $(document).on('click', 'a.buy', function(e) {
+        var $target = $(e.target);
+        var goal = $target.data('goal');
+        ga('send', 'event', 'Purchases', 'click', goal);
+    });
+
     $('.fancybox').fancybox({
         prevEffect		: 'none',
         nextEffect		: 'none',
