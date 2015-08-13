85e6f9b9a5f1ec4682fd34ca8432dd7208a797b4
Dave Elkan <dave@edave.net>
2015-08-13 08:19:48 -0700

    Check for cookie

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,7 +1,7 @@
 $(function() {
   var JSP_POPUP = "jspPopup";
   var hasCookie = document.cookie.indexOf(JSP_POPUP) >= 0;
-  //if (!hasCookie) {
+  if (!hasCookie) {
     setTimeout(function() {
       var expires = new Date();
       expires.setTime(+ expires + (90 * 86400000));
@@ -26,5 +26,5 @@ $(function() {
         ga('send', 'event', 'Samples', 'popup closed', 'Popup closed');
       });
     }, 15000);
-  //}
+  }
 });
